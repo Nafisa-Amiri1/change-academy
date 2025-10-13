10cf694a5dcff93f710cadc8d6cd31db81d565ff
Nafisa-Amiri1 <nafisaamiri107@gmail.com>
2025-10-13 11:30:17 +0430

    feat(chage-acdemy): clone repository

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Change Academy',
+    default: 'Change Academy - Developers, Designers, and Growth Opportunities',
+  },
+  description:
+    'Change Academy is a team of developers, designers, and tech specialists. We create projects, help aspiring programmers grow, and connect talent with opportunities worldwide.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
