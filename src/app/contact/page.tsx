d9299f829d09f985e0cb2678a2bf2fde7afe151b
HakimaNoori <hakima.noori.1379@gmail.com>
2025-10-18 11:35:13 +0430

    changes color code

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -190,7 +190,7 @@ export default function Projects() {
         <div className="mt-10">
           <button
             type="submit"
-            className="block w-full rounded-md bg-teal-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-teal-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500"
+            className="block w-full rounded-md bg-teal-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500"
           >
             Let's talk
           </button>
